36268c9ca5ed230c37faed5c50348c5a1ce52803
Sudesh Mate <sudesh7443@gmail.com>
2023-04-02 14:03:03 +0545

    feat: make nav bar responsive and start carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
     </div>
   );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,26 +5,37 @@ import { Icon } from "@iconify/react";
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <ul className="navbar">
-          <li className="menu">
-            <Icon icon="ant-design:menu-outlined" width="26px" />
-            <span>Menu</span>
+      <nav className="navbar">
+        <span className="menu">
+          <Icon icon="ant-design:menu-outlined" width="26px" />
+          <span>Menu</span>
+        </span>
+        <span className="title">
+          <h5>The</h5>
+          <h1>Wasdale</h1>
+          <h5>Head Inn</h5>
+        </span>
+        <ul className="interactions">
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Icon icon="ri:facebook-box-fill" />
+            </a>
           </li>
-          <li className="title">
-            <h5>The</h5>
-            <h1>Wasdale</h1>
-            <h5>Head Inn</h5>
+
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Icon icon="ri:instagram-fill" />
+            </a>
           </li>
-          <li className="interactions">
-            <Icon icon="ri:facebook-box-fill" />
 
-            <Icon icon="ri:instagram-fill" />
-
-            <Icon icon="ant-design:twitter-square-filled" />
-
-            <span className="book">Book now !!</span>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Icon icon="ant-design:twitter-square-filled" />
+            </a>
           </li>
+          <button className="book" onClick={() => console.log("Book now")}>
+            Book now !!
+          </button>
         </ul>
       </nav>
     </>
